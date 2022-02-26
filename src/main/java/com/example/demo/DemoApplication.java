fe6e40ce52ff27b5377a82d37f428f5ac2771dad
Mark Stachowski <mstachowski@globalgiving.org>
2022-02-26 11:13:20 -0500

    Minor update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,18 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @SpringBootApplication
 public class DemoApplication {
 
-    @RequestMapping("/")
-    String home() {
-        return "IgnitePOC!";
-    }
+  @RequestMapping("/")
+  String home() {
+    return "IgnitePOC!";
+  }
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 }
